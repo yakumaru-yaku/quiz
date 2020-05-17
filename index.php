35227fffa993f3a85ec1314a5de4a555cccdfa62
yakumaru-yaku <yakumaru.engineer@gmail.com>
2020-05-18 03:35:01 +0900

    z

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ shuffle($quiz['a']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>!簡単クaイズ!!</title>
+    <title>!簡単クaaイズ!!</title>
 </head>
 <body>
 <div style="padding: 7px; background: #eee; border: #ccc;">
